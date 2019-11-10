201d6ef76b677e8fab0115a3de9820f8a13c622c
febrijumawan27 <57055098+febrijumawan27@users.noreply.github.com>
2019-11-10 13:23:20 +0700

    Delete lat 1.py

--- lat 1.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Masukan nilai N: "))
-
-jumlah = n
-from random import random
-for i in range (jumlah):
-    from random import random
-    data = random()
-    print("data ke :",i,(data))
-print("selesai")
