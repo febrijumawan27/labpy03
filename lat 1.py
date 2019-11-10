84f01a1ea581410d8e784b7495403e8ac820f16a
febrijumawan27 <57055098+febrijumawan27@users.noreply.github.com>
2019-11-10 13:24:58 +0700

    Add files via upload

---
+++ lat 1.py
@@ -0,0 +1,9 @@
+n = int(input("Masukan nilai N: "))
+
+jumlah = n
+import random
+for i in range (n):
+    data=random.uniform(0,0.5)
+    i+=1
+    print("data ke :",i,"==>",(data))
+print("selesai")
